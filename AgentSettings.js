c7ae39494167fb55ee23ce1430fa2a9dcae8979b
PIPKIN <107678789+XMGank@users.noreply.github.com>
2026-07-12 10:33:39 +0800

    Add files via upload

---
+++ AgentSettings.js
@@ -0,0 +1,9 @@
+window.ZHIHAO_AGENT_SETTINGS = {
+  enabled: false,
+  endpoint: "",
+  method: "POST",
+  apiKey: "",
+  apiKeyHeader: "Authorization",
+  apiKeyPrefix: "Bearer ",
+  timeoutMs: 30000
+};
